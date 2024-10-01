3e4c11e2a3aaa02d6bd72365aae1b6b24f29ac44
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2024-10-02 04:19:59 +0600

    Search Filter implicated

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -6,15 +6,32 @@ import { IoIosArrowDown } from "react-icons/io";
 
 const Jobs = () => {
     const jobs = useLoaderData()
-    const [appliedJobs, setAppliedJobs] = useState([])
+    const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
 
     useEffect(() => {
         const storedJobIds = getStoredJobApplication();
         if (jobs.length) {
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id))
             setAppliedJobs(jobsApplied)
+            setDisplayJobs(jobsApplied)
+
         }
     }, [])
+
+    const handleFilterJob = filter => {
+        if (filter === 'all') {
+            setDisplayJobs(appliedJobs)
+        }
+        else if (filter === 'remote') {
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote')
+            setDisplayJobs(remoteJobs)
+        }
+        else if (filter === 'onsite') {
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite')
+            setDisplayJobs(onsiteJobs)
+        }
+    }
 
     return (
         <div className="my-12">
@@ -22,14 +39,14 @@ const Jobs = () => {
                 <details className="dropdown dropdown-left">
                     <summary className="btn m-1">Filter <IoIosArrowDown size="24" /></summary>
                     <ul className="menu dropdown-content bg-base-200 rounded-box z-[1] w-52 p-2 shadow">
-                        <li><a>All</a></li>
-                        <li><a>Remote</a></li>
-                        <li><a>On site</a></li>
+                        <li onClick={() => handleFilterJob('all')}><a>All</a></li>
+                        <li onClick={() => handleFilterJob('remote')}><a>Remote</a></li>
+                        <li onClick={() => handleFilterJob('onsite')}><a>On site</a></li>
                     </ul>
                 </details>
             </div>
             <div className="space-y-6">
-                {appliedJobs.map(job => <Job key={job.id} job={job}></Job>)}
+                {displayJobs.map(job => <Job key={job.id} job={job}></Job>)}
             </div>
         </div>
     );

737ae2c681c01a7691b00b29c90202bb0a15a13d
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2024-10-02 04:01:29 +0600

    Jobs page development and made responsive for all devices

---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import { AiOutlineDollar } from 'react-icons/ai';
+import { IoLocationOutline } from 'react-icons/io5';
+const Job = ({ job }) => {
+    const { id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
+    return (
+        <div className="card lg:card-side bg-base-100 p-7 border">
+            <figure>
+                <img className='bg-gray-100 p-8 rounded-2xl object-cover lg:w-64 '
+                    src={logo}
+                    alt={company_name} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div className='flex'>
+                    <button className='font-extrabold px-5 py-2 border-2 rounded border-violet-500 text-violet-700 mr-4'>{remote_or_onsite}</button>
+                    <button className='font-extrabold px-5 py-2 border-2 rounded border-violet-500 text-violet-700'>{job_type}</button>
+                </div>
+                <div className='flex'>
+                    <p className='flex items-center text-lg text-gray-600'>
+                        <IoLocationOutline className='text-2xl mr-2' /> {location}
+                    </p>
+                    <p className='flex items-center text-lg text-gray-600'>
+                        <AiOutlineDollar className='text-2xl mr-2' /> Salary: {salary}
+                    </p>
+                </div>
+            </div>
+            <div className='flex items-center'>
+                <button className='btn btn-primary'>View Details</button>
+            </div>
+        </div>
+    );
+};
+Job.propTypes = {
+    job: PropTypes.object,
+}
+
+export default Job;
--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,21 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../utility/localStorage";
+import Job from '../components/Job/Job'
+import { IoIosArrowDown } from "react-icons/io";
 
 const Jobs = () => {
     const jobs = useLoaderData()
+    const [appliedJobs, setAppliedJobs] = useState([])
 
     useEffect(() => {
         const storedJobIds = getStoredJobApplication();
         if (jobs.length) {
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id))
-            console.log(jobsApplied);
+            setAppliedJobs(jobsApplied)
         }
     }, [])
 
     return (
-        <div>
-            <h1 className="3xl">Jobs i applied for</h1>
+        <div className="my-12">
+            <div className="flex justify-end mb-8">
+                <details className="dropdown dropdown-left">
+                    <summary className="btn m-1">Filter <IoIosArrowDown size="24" /></summary>
+                    <ul className="menu dropdown-content bg-base-200 rounded-box z-[1] w-52 p-2 shadow">
+                        <li><a>All</a></li>
+                        <li><a>Remote</a></li>
+                        <li><a>On site</a></li>
+                    </ul>
+                </details>
+            </div>
+            <div className="space-y-6">
+                {appliedJobs.map(job => <Job key={job.id} job={job}></Job>)}
+            </div>
         </div>
     );
 };
